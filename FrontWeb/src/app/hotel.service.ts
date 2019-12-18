c774a4678341341ecff7fcdc1e20c7b1ecf22ee3
Yacouba <jameleco@yahoo.fr>
2019-12-18 16:30:55 +0100

    Add-Update-Delete

--- FrontWeb/src/app/hotel.service.ts
+++ FrontWeb/src/app/hotel.service.ts
@@ -8,6 +8,9 @@ import { Hotel } from './model/hotel';
 })
 export class HotelService {
   private apiUrl ='https://localhost:44374/api/hotels';
+  private httpOptions = {
+    headers : new HttpHeaders({'content-type':'application/json'})
+  };
 
   constructor(
     private http: HttpClient) { }
@@ -17,5 +20,21 @@ export class HotelService {
     getHotel(hotetId: number) : Observable<Hotel>{
       return this.http.get<Hotel>(`${this.apiUrl}/${hotetId}`);
 
+    }
+
+    addHotel(hotel : Hotel){
+      return this.http.post<Hotel>(this.apiUrl,hotel,this.httpOptions);
+
+    }
+    updateHotel(hotel : Hotel): Observable<any>{
+      const url = `${this.apiUrl}/${hotel.hotelId}`;
+      return this.http.put<Hotel>(url, hotel, this.httpOptions);
+
+    }
+    deleteHotel(hotel: Hotel | number) : Observable<Hotel>{
+      const id: number = typeof hotel === 'number' ? hotel : hotel.hotelId;
+      const url = `${this.apiUrl}/${id}`;
+
+      return this.http.delete<Hotel>(url);
     }
 }
